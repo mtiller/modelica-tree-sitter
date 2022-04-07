2241b6cdc61d157249531350f6b82539088838c8
Michael Tiller <michael.tiller@gmail.com>
2022-04-06 21:06:51 -0400

    tests: added corpus directory for testing

---
+++ queries/highlights.scm
@@ -0,0 +1,50 @@
+(type_specifier) @type
+(STRING) @string
+(comment) @comment
+(component_reference) @property
+(class_specifier name: (IDENT) @function)
+(class_specifier endname: (IDENT) @function)
+(modification (primary_expression (STRING) @injection.content))
+
+(class) @keyword
+(model) @keyword
+(record) @keyword
+(block) @keyword
+(connector) @keyword
+(type) @keyword
+(package) @keyword
+(function) @keyword
+(operator) @keyword
+
+(extends) @keyword
+(encapsulated) @keyword
+(partial) @keyword
+(final) @keyword
+(operator) @keyword
+(expandable) @keyword
+(pure) @keyword
+(impure) @keyword
+(flow) @keyword
+(stream) @keyword
+(discrete) @keyword
+(parameter) @keyword
+(constant) @keyword
+(input) @keyword
+(output) @keyword
+(each) @keyword
+(public) @keyword
+(protected) @keyword
+(external) @keyword
+(enumeration) @keyword
+(redeclare) @keyword
+(replaceable) @keyword
+(inner) @keyword
+(outer) @keyword
+(constrainedby) @keyword
+(initial) @keyword
+(break) @keyword
+(return) @keyword
+
+"equation" @keyword
+"algorithm" @keyword
+"end" @keyword
